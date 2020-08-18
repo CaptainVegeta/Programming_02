6169dfc8e9e4b76fa971dc9545660c9cd75c2ece
Murat Tokmakci <murat.tokmakci@outlook.com>
2020-08-18 12:57:55 +0200

    minor changes

--- 01_Abstrakte_Klassen_und_Schnittstellen/FolienAufgaben/Aufgabe_01/Hauptklasse.java
+++ 01_Abstrakte_Klassen_und_Schnittstellen/FolienAufgaben/Aufgabe_01/Hauptklasse.java
@@ -2,9 +2,12 @@
 public class Hauptklasse {
     public static void main(String[] args) {
         
+        /*
         Erben instanz_02 = new Erben();
         instanz_02.extending();
-        // HalloWelt instanz_01 = new HalloWelt();
-        // System.out.println(instanz_01);
+        */
+
+        HalloWelt instanz_01 = new HalloWelt();
+        System.out.println(instanz_01);
     }
 }
